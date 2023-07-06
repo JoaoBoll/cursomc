27fd8c53b5a77cabdd8624bd02268476918a36e9
João Boll <joaovitorpianista@hotmail.com>
2023-07-06 12:43:10 -0300

    Adicionado Banco de dados

---
+++ src/main/java/com/example/cursomc/domain/Categoria.java
@@ -0,0 +1,27 @@
+package com.example.cursomc.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+}
--- src/main/java/com/example/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/example/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,32 @@
 package com.example.cursomc.resources;
 
+import com.example.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "Deu certo";
+    public List<Categoria> listar() {
+
+        Categoria categoria1 = new Categoria(1, "Informática");
+        Categoria categoria2 = new Categoria(1, "Informática");
+
+        List<Categoria> ls = new ArrayList<>();
+
+        ls.add(categoria1);
+        ls.add(categoria2);
+
+        //testando
+
+        ls.add(new Categoria(3, "TESTEEE"));
+        return ls;
     }
 
 }
