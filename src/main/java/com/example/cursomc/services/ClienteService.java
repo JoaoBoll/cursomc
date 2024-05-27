ba2ebd7d9c65f25b95a8eb7ec7a108ee95bec12a
João <joaovitorboll@outlook.com.br>
2024-05-27 18:38:37 -0300

    Ajustado EndPoind cliente, ao deletar, mensagem alterada caso erro!

--- src/main/java/com/example/cursomc/services/ClienteService.java
+++ src/main/java/com/example/cursomc/services/ClienteService.java
@@ -68,7 +68,7 @@ public class ClienteService {
         try {
             clienteRepository.deleteById(id);
         } catch (DataIntegrityViolationException e) {
-            throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+            throw new DataIntegrityException("Não é possível excluir um cliente que possui pedidos registrados.");
         }
 
     }
